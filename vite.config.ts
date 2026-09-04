ddb2ac86e36151601461fe85d552e75b9674bcda
Bryant Wong <bryanthwong@gmail.com>
2026-09-03 22:46:58 -0400

    added tailwind

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,13 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+    tailwindcss(),
     vueDevTools(),
   ],
   resolve: {
@@ -15,5 +17,5 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
-  base: '/no-to-war/',
+  base: '/no-to-war/'
 })
